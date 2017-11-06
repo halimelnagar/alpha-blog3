62ec5443efe1100b9297604d45acf398e0bc016d
halimelnagar <halim.elnagar@gmail.com>
2017-11-06 20:36:08 +0000

    Create homepage and add links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -47,7 +47,7 @@ class ArticlesController < ApplicationController
 		#@article = Article.find(params[:id])
 		@article.destroy
 		flash[:notice] = "Article destroyed successfully"
-		
+		redirect_to articles_path
 	end
 
 
