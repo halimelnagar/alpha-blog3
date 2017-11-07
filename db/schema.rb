ba6944bb159ef53e183a4393409fb8e333fbf0d1
halimelnagar <halim.elnagar@gmail.com>
2017-11-07 20:17:26 +0000

    add user validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171104152318) do
+ActiveRecord::Schema.define(version: 20171107061019) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text   "description"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
